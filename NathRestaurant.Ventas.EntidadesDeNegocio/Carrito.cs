503821fd368c202c750329bb386b77ba9e16c559
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-23 21:59:38 -0600

    Creacion de la clase Carrito de la capa entidades de negocio

---
+++ NathRestaurant.Ventas.EntidadesDeNegocio/Carrito.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NathRestaurant.Ventas.EntidadesDeNegocio
+{
+    public class Carrito
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [ForeignKey("Producto")]
+        public int IdProducto { get; set; }
+
+        [ForeignKey("Cliente")]
+        public int IdCliente { get; set; }
+
+        public Producto? Producto { get; set; }
+
+        public Cliente? Cliente { get; set; }
+    }
+}
