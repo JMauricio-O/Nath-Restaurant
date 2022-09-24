6e6663f68849093432d30e02dbd028f2dd97be07
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-23 21:34:07 -0600

    Creacion de la entidad Categoria

--- NathRestaurant.Ventas.EntidadesDeNegocio/Categoria.cs
+++ NathRestaurant.Ventas.EntidadesDeNegocio/Categoria.cs
@@ -1,29 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace NathRestaurant.Ventas.EntidadesDeNegocio
 {
-    
-        public class Categoria
-        {
 
-            [Key]
-            public int Id { get; set; }
+    public class Categoria
+    {
 
-            [Display(Name = "Nombre Categoria")]
-            [StringLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
-            [Required(ErrorMessage = "El campo {0} es requerido")]
-            public string? Nombre { get; set; }
+        [Key]
+        public int Id { get; set; }
 
-            [NotMapped]
-            public int Top_Aux { get; set; }
+        [Display(Name = "Nombre Categoria")]
+        [StringLength(50, ErrorMessage = "{0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "{0} es requerido")]
+        public string? Nombre { get; set; }
 
-            public List<Producto>? Productos { get; set; }
-        }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+
+        public List<Producto>? Productos { get; set; }
+    }
 }
