72942693230a254a2608b275df06511d17a1d7da
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-24 17:23:14 -0600

    Actualizacion de la clase RolDAL

--- NathRestaurant.Ventas.AccesoADatos/RolDAL.cs
+++ NathRestaurant.Ventas.AccesoADatos/RolDAL.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NathRestaurant.Ventas.EntidadesDeNegocio;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NathRestaurant.Ventas.AccesoADatos
 {
     public class RolDAL
     {
-        public static async Task<int>AgregarAsync(Rol pRol)
+        public static async Task<int> AgregarAsync(Rol pRol)
         {
             int resul = 0;
             using (var dbContext = new DBContext())
@@ -34,7 +29,7 @@ namespace NathRestaurant.Ventas.AccesoADatos
             return resul;
         }
 
-        public static async Task<int>EliminarAsync(Rol pRol)
+        public static async Task<int> EliminarAsync(Rol pRol)
         {
             var resul = 0;
             using (var dbContext = new DBContext())
@@ -44,6 +39,56 @@ namespace NathRestaurant.Ventas.AccesoADatos
                 resul = await dbContext.SaveChangesAsync();
             }
             return resul;
+        }
+
+        public static async Task<Rol> ObtenerPorIdAsync(Rol pRol)
+        {
+            var rol = new Rol();
+            using (var dbContext = new DBContext())
+            {
+                rol = await dbContext.Rol.FirstOrDefaultAsync(r => r.Id == pRol.Id);
+            }
+            return rol;
+        }
+
+        public static async Task<List<Rol>> ObtenerTodosAsync()
+        {
+            var rol = new List<Rol>();
+            using (var dbContext = new DBContext())
+            {
+                rol = await dbContext.Rol.ToListAsync();
+            }
+            return rol;
+        }
+
+        internal static IQueryable<Rol> QuerySelect(IQueryable<Rol> pQuery, Rol pRol)
+        {
+            if (pRol.Id > 0)
+            {
+                pQuery = pQuery.Where(r => r.Id == pRol.Id);
+            }
+            if (!string.IsNullOrWhiteSpace(pRol.Nombre))
+            {
+                pQuery = pQuery.Where(r => r.Nombre.Contains(pRol.Nombre));
+            }
+            pQuery = pQuery.OrderByDescending(r => r.Id == pRol.Id).AsQueryable();
+            if (pRol.Top_Aux > 0)
+            {
+                pQuery = pQuery.Take(pRol.Top_Aux).AsQueryable();
+            }
+            return pQuery;
+        }
+
+        public static async Task<List<Rol>> BuscarAsync(Rol pRol)
+        {
+            var rol = new List<Rol>();
+            using (var dbContext = new DBContext())
+            {
+                var select = dbContext.Rol.AsQueryable();
+                select = QuerySelect(select, pRol);
+                rol = await select.ToListAsync();
+            }
+            return rol;
         }
     }
 }
