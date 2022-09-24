89a25459e4e87d46d8da3672c1a98c2eaec11222
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-23 22:08:28 -0600

    Creacion de la clase DetallaVenta de la capa de entidades de negocio

---
+++ NathRestaurant.Ventas.EntidadesDeNegocio/DetalleVenta.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NathRestaurant.Ventas.EntidadesDeNegocio
+{
+    public class DetalleVenta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Producto")]
+        [Required]
+        public int IdProducto { get; set; }
+
+        [Required]
+        [DataType("integer")]
+        public int Cantidad { get; set; }
+
+        [Required]
+        public double Subtotal { get; set; }
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+
+        public Producto? Producto { get; set; }
+    }
+}
