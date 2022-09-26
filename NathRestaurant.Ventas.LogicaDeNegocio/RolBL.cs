26d02c9d96e0e6701ff63bf15037887f95aeabe9
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-25 22:43:18 -0600

    Creacion de la clase RolBL

---
+++ NathRestaurant.Ventas.LogicaDeNegocio/RolBL.cs
@@ -0,0 +1,33 @@
+﻿using NathRestaurant.Ventas.AccesoADatos;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+
+namespace NathRestaurant.Ventas.LogicaDeNegocio
+{
+    public class RolBL
+    {
+        public async Task<int> AgregarAsync(Rol pRol)
+        {
+            return await RolDAL.AgregarAsync(pRol);
+        }
+        public async Task<int> ModificarAsync(Rol pRol)
+        {
+            return await RolDAL.ModificarAsync(pRol);
+        }
+        public async Task<int> EliminarAsync(Rol pRol)
+        {
+            return await RolDAL.EliminarAsync(pRol);
+        }
+        public async Task<Rol> ObtenerPorIdAsync(Rol pRol)
+        {
+            return await RolDAL.ObtenerPorIdAsync(pRol);
+        }
+        public async Task<List<Rol>> ObtenerTodosAsync()
+        {
+            return await RolDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<Rol>> BuscarAsync(Rol pRol)
+        {
+            return await RolDAL.BuscarAsync(pRol);
+        }
+    }
+}
