f52e3e7c6ebe3d15dfc041a534b99ceb26abf343
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-30 18:30:18 -0600

    Creacion de la clase CarritoBL

---
+++ NathRestaurant.Ventas.LogicaDeNegocio/CarritoBL.cs
@@ -0,0 +1,33 @@
+﻿using NathRestaurant.Ventas.AccesoADatos;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+
+namespace NathRestaurant.Ventas.LogicaDeNegocio
+{
+    public class CarritoBL
+    {
+        public async Task<int> AgregarAsync(Carrito pCarrito)
+        {
+            return await CarritoDAL.AgregarAsync(pCarrito);
+        }
+        public async Task<int> ModificarAsync(Carrito pCarrito)
+        {
+            return await CarritoDAL.ModificarAsync(pCarrito);
+        }
+        public async Task<int> EliminarAsync(Carrito pCarrito)
+        {
+            return await CarritoDAL.EliminarAsync(pCarrito);
+        }
+        public async Task<Carrito> ObtenerPorIdAsync(Carrito pCarrito)
+        {
+            return await CarritoDAL.ObtenerPorIdAsync(pCarrito);
+        }
+        public async Task<List<Carrito>> ObtenerTodosAsync()
+        {
+            return await CarritoDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<Carrito>> BuscarAsync(Carrito pCarrito)
+        {
+            return await CarritoDAL.BuscarAsync(pCarrito);
+        }
+    }
+}
