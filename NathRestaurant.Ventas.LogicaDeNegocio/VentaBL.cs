e62032f34eb7eb5c9ee9fa4c193bd98bdc6a4141
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-30 18:54:41 -0600

    Creacion de la clase VentaBL

---
+++ NathRestaurant.Ventas.LogicaDeNegocio/VentaBL.cs
@@ -0,0 +1,37 @@
+﻿using NathRestaurant.Ventas.AccesoADatos;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+
+namespace NathRestaurant.Ventas.LogicaDeNegocio
+{
+    public class VentaBL
+    {
+        public async Task<int> AgregarAsync(Venta pVenta)
+        {
+            return await VentaDAL.AgregarAsync(pVenta);
+        }
+        public async Task<int> ModificarAsync(Venta pVenta)
+        {
+            return await VentaDAL.ModificarAsync(pVenta);
+        }
+        public async Task<int> EliminarAsync(Venta pVenta)
+        {
+            return await VentaDAL.EliminarAsync(pVenta);
+        }
+        public async Task<Venta> ObtenerPorIdAsync(Venta pVenta)
+        {
+            return await VentaDAL.ObtenerPorIdAsync(Venta pVenta)
+        }
+        public async Task<List<Venta>> ObtenerTodosAsync()
+        {
+            return await VentaDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<Venta>> BuscarAsync(Venta pVenta)
+        {
+            return await VentaDAL.BuscarAsync(pVenta);
+        }
+        public async Task<List<Venta>> BuscarIncluirTodosAsync(Venta pVenta)
+        {
+            return await VentaDAL.BuscarIncluirTodosAsync(pVenta);
+        }
+    }
+}
