613591624aa5b3621091dad165e285c2df688dae
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-30 19:12:54 -0600

    Creacion del API Controller de Combos y Configuraciones

--- NathRestaurant.Ventas.WebAPI/Controllers/CombosController.cs
+++ NathRestaurant.Ventas.WebAPI/Controllers/CombosController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+using NathRestaurant.Ventas.LogicaDeNegocio;
+using System.Text.Json;
 
 namespace NathRestaurant.Ventas.WebAPI.Controllers
 {
@@ -7,5 +9,77 @@ namespace NathRestaurant.Ventas.WebAPI.Controllers
     [ApiController]
     public class CombosController : ControllerBase
     {
+        private CombosBL _combosBL = new CombosBL();
+
+        [HttpGet]
+        public async Task<IEnumerable<Combos>> Get()
+        {
+            return await _combosBL.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Combos> Get(int id)
+        {
+            Combos combos = new Combos();
+            combos.Id = id;
+            return await _combosBL.ObtenerPorIdAsync(combos);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Combos combos)
+        {
+            try
+            {
+                await _combosBL.AgregarAsync(combos);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Combos combos)
+        {
+
+            if (combos.Id == id)
+            {
+                await _combosBL.ModificarAsync(combos);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Combos combos = new Combos();
+                combos.Id = id;
+                await _combosBL.EliminarAsync(combos);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Combos>> Buscar([FromBody] object pCombos)
+        {
+
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strCombos = JsonSerializer.Serialize(pCombos);
+            Combos combos = JsonSerializer.Deserialize<Combos>(strCombos, option);
+            return await _combosBL.BuscarAsync(combos);
+        }
     }
 }
--- NathRestaurant.Ventas.WebAPI/Controllers/ConfiguracionController.cs
+++ NathRestaurant.Ventas.WebAPI/Controllers/ConfiguracionController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+using NathRestaurant.Ventas.LogicaDeNegocio;
+using System.Text.Json;
 
 namespace NathRestaurant.Ventas.WebAPI.Controllers
 {
@@ -7,5 +9,78 @@ namespace NathRestaurant.Ventas.WebAPI.Controllers
     [ApiController]
     public class ConfiguracionController : ControllerBase
     {
+        private ConfiguracionesBL _configuracionesBL = new ConfiguracionesBL();
+
+        [HttpGet]
+        public async Task<IEnumerable<Configuracion>> Get()
+        {
+            return await _configuracionesBL.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Configuracion> Get(int id)
+        {
+            Configuracion configuracion = new Configuracion();
+            configuracion.Id = id;
+            return await _configuracionesBL.ObtenerPorIdAsync(configuracion);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Configuracion configuracion)
+        {
+            try
+            {
+                await _configuracionesBL.AgregarAsync(configuracion);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Configuracion configuracion)
+        {
+
+            if (configuracion.Id == id)
+            {
+                await _configuracionesBL.ModificarAsync(configuracion);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Configuracion configuracion = new Configuracion();
+                configuracion.Id = id;
+                await _configuracionesBL.EliminarAsync(configuracion);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Configuracion>> Buscar([FromBody] object pConfiguracion)
+        {
+
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strConfiguracion = JsonSerializer.Serialize(pConfiguracion);
+            Configuracion configuracion = JsonSerializer.Deserialize<Configuracion>(strConfiguracion, option);
+            return await _configuracionesBL.BuscarAsync(configuracion);
+        }
     }
+}
 }
