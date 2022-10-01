cd45c43f33306b921eb490bf98c303ec75e216d9
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-30 18:35:54 -0600

    Creacion de la clase ClienteBL

---
+++ NathRestaurant.Ventas.LogicaDeNegocio/ClienteBL.cs
@@ -0,0 +1,42 @@
+﻿using NathRestaurant.Ventas.AccesoADatos;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+
+namespace NathRestaurant.Ventas.LogicaDeNegocio
+{
+    public class ClienteBL
+    {
+        public async Task<int> AgregarAsync(Cliente pCliente)
+        {
+            return await ClienteDAL.AgregarAsync(pCliente);
+        }
+        public async Task<int> ModificarAsync(Cliente pCliente)
+        {
+            return await ClienteDAL.ModificarAsync(pCliente);
+        }
+        public async Task<int> EliminarAsync(Cliente pCliente)
+        {
+            return await ClienteDAL.EliminarAsync(pCliente);
+        }
+        public async Task<Cliente> ObtenerPorIdAsync(Cliente pCliente)
+        {
+            return await ClienteDAL.ObtenerPorIdAsync(pCliente);
+        }
+        public async Task<List<Cliente>> ObtenerTodosAsync()
+        {
+            return await ClienteDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<Cliente>> BuscarAsync(Cliente pCliente)
+        {
+            return await ClienteDAL.Buscar(pCliente);
+        }
+        public async Task<Cliente> Login(Cliente pCliente)
+        {
+            return await ClienteDAL.Login(pCliente);
+        }
+
+        public async Task<int> CambiarContrasenia(Cliente pCliente, string pContraseña)
+        {
+            return await ClienteDAL.CambiarContrasenia(pCliente, pContraseña);
+        }
+    }
+}
