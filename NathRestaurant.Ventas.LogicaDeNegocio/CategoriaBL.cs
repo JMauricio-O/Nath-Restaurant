3830f4cf6b5b2e207a79af5c352c3e7615175b5a
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-30 18:23:02 -0600

    Creacion del API controller Categoria

---
+++ NathRestaurant.Ventas.LogicaDeNegocio/CategoriaBL.cs
@@ -0,0 +1,33 @@
+﻿using NathRestaurant.Ventas.AccesoADatos;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+
+namespace NathRestaurant.Ventas.LogicaDeNegocio
+{
+    public class CategoriaBL
+    {
+        public async Task<int> AgregarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.AgregarAsync(pCategoria);
+        }
+        public async Task<int> ModificarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.ModificarAsync(pCategoria);
+        }
+        public async Task<int> EliminarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.EliminarAsync(pCategoria);
+        }
+        public async Task<Categoria> ObtenerPorIdAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.ObtenerPorIdAsync(pCategoria);
+        }
+        public async Task<List<Categoria>> ObtenerTodosAsync()
+        {
+            return await CategoriaDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<Categoria>> BuscarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.BuscarAsync(pCategoria);
+        }
+    }
+}
--- NathRestaurant.Ventas.WebAPI/Controllers/CategoriaController.cs
+++ NathRestaurant.Ventas.WebAPI/Controllers/CategoriaController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+using NathRestaurant.Ventas.LogicaDeNegocio;
+using System.Text.Json;
 
 namespace NathRestaurant.Ventas.WebAPI.Controllers
 {
@@ -7,6 +9,77 @@ namespace NathRestaurant.Ventas.WebAPI.Controllers
     [ApiController]
     public class CategoriaController : ControllerBase
     {
+        private CategoriaBL _CategoriaBL = new CategoriaBL();
 
+        [HttpGet]
+        public async Task<IEnumerable<Categoria>> Get()
+        {
+            return await _CategoriaBL.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Categoria> Get(int id)
+        {
+            Categoria categoria = new Categoria();
+            categoria.Id = id;
+            return await _CategoriaBL.ObtenerPorIdAsync(categoria);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Categoria categoria)
+        {
+            try
+            {
+                await _CategoriaBL.AgregarAsync(categoria);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Categoria categoria)
+        {
+
+            if (categoria.Id == id)
+            {
+                await _CategoriaBL.ModificarAsync(categoria);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Categoria categoria = new Categoria();
+                categoria.Id = id;
+                await _CategoriaBL.EliminarAsync(categoria);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Categoria>> Buscar([FromBody] object pCategoria)
+        {
+
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strCategoria = JsonSerializer.Serialize(pCategoria);
+            Categoria categoria = JsonSerializer.Deserialize<Categoria>(strCategoria, option);
+            return await _CategoriaBL.BuscarAsync(categoria);
+        }
     }
 }
