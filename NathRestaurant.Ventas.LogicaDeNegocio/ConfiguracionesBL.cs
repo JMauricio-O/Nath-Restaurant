a592d7fd02584ce673aefaf2f445582debee7729
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-30 18:26:46 -0600

    Creacion de la clase ConfiguracionBL

---
+++ NathRestaurant.Ventas.LogicaDeNegocio/ConfiguracionesBL.cs
@@ -0,0 +1,33 @@
+﻿using NathRestaurant.Ventas.AccesoADatos;
+using NathRestaurant.Ventas.EntidadesDeNegocio;
+
+namespace NathRestaurant.Ventas.LogicaDeNegocio
+{
+    public class ConfiguracionesBL
+    {
+        public async Task<int> AgregarAsync(Configuracion pConfiguracion)
+        {
+            return await ConfiguracionesDAL.AgregarAsync(pConfiguracion);
+        }
+        public async Task<int> ModificarAsync(Configuracion pConfiguracion)
+        {
+            return await ConfiguracionesDAL.ModificarAsync(pConfiguracion);
+        }
+        public async Task<int> EliminarAsync(Configuracion pConfiguracion)
+        {
+            return await ConfiguracionesDAL.EliminarAsync(pConfiguracion);
+        }
+        public async Task<Configuracion> ObtenerPorIdAsync(Configuracion pConfiguracion)
+        {
+            return await ConfiguracionesDAL.ObtenerPorIdAsync(pConfiguracion);
+        }
+        public async Task<List<Configuracion>> ObtenerTodosAsync()
+        {
+            return await ConfiguracionesDAL.ObtenerTodosAsync();
+        }
+        public async Task<List<Configuracion>> BuscarAsync(Configuracion pConfiguracion)
+        {
+            return await ConfiguracionesDAL.BuscarAsync(pConfiguracion);
+        }
+    }
+}
